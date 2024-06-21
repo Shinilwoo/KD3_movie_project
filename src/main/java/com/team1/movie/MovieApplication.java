48309023d36679e46afeb191729f561a99bb97b1
ad.seoil <ad.seoil0112@gmail.com>
2024-06-21 15:51:19 +0900

    test develop brunch

--- src/main/java/com/team1/movie/MovieApplication.java
+++ src/main/java/com/team1/movie/MovieApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieApplication {
 
+	// 테스트용
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
 	}
