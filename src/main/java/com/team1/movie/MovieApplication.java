2d9928991545ba03e9032e350cc0263e3131552a
ad.seoil <ad.seoil0112@gmail.com>
2024-06-21 15:52:35 +0900

    브런치 테스트

--- src/main/java/com/team1/movie/MovieApplication.java
+++ src/main/java/com/team1/movie/MovieApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MovieApplication {
 
 	// 테스트용
+	// 테스트용 2
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
 	}
